685668c9015f2a107a729980abe45e34ef8f6403
GoodMan <15626404376@163.com>
2019-01-01 16:00:08 +0800

    初次提交

---
+++ src/main/java/com/interestedman/hihelpbytheway/HiHelpBythewayApplication.java
@@ -0,0 +1,14 @@
+package com.interestedman.hihelpbytheway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiHelpBythewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiHelpBythewayApplication.class, args);
+    }
+
+}
+
